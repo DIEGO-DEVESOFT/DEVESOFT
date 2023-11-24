e4242aa5cc3648afa15196cf0e3e10722acc8f3b
DIEGO-DEVESOFT <diegogonzalezprogramador@gmail.com>
2023-11-23 21:35:17 -0500

    coloque los links de las redes sociales

--- Controllers/Landing.php
+++ Controllers/Landing.php
@@ -3,22 +3,23 @@
     class Landing{
         public function __construct(){}
         public function index(){
-            if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-                echo "<script>alert ('MENSAJE ENVIADO')</script>";
-                require_once "views/Company/header.view.php";
-                require_once "views/Company/index.view.php";  
-                require_once "views/Company/footer.view.php";
-            }
-            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-                $Pregunta = new Pregunta(
-                    NULL,
-                    $_POST['NombreCompleto'],
-                    $_POST['Email'],
-                    $_POST['Pregunta']
-                );
-                $Pregunta->RegistrarPregunta();
-                header("Location: ?c=Landing&a=index");
-            } 
+            
+                if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+                    // echo "<script>alert ('MENSAJE ENVIADO')</script>";
+                    require_once "views/Company/header.view.php";
+                    require_once "views/Company/index.view.php";  
+                    require_once "views/Company/footer.view.php";
+                }
+                if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                    $Pregunta = new Pregunta(
+                        NULL,
+                        $_POST['NombreCompleto'],
+                        $_POST['Email'],
+                        $_POST['Pregunta']
+                    );
+                    $Pregunta->RegistrarPregunta();
+                    header("Location: ?c=Landing&a=index");
+                } 
         }
     }
 ?>
